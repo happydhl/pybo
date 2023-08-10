917f351d4d50072872140fc1e581a55534febabe
happydhl <happydhl@naver.com>
2023-08-10 13:56:09 +0900

    view.py 수정

--- common/views.py
+++ common/views.py
@@ -19,6 +19,6 @@ def signup(request):
     return render(request, 'common/signup.html', {'form': form})
 
 def page_not_found(request, exception):
-    return render(request, 'common/404.html' {})
+    return render(request, 'common/404.html', {})
 
 
